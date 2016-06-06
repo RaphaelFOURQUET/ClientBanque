1b97de0ae99df5c4eb1b6e9a8097095f0709b639
RaphaelFOURQUET <raphikifourque@gmail.com>
2016-06-06 16:57:48 +0200

    Delete TODO

--- ClientBanque/src/fr/adaming/ConversionServlet.java
+++ ClientBanque/src/fr/adaming/ConversionServlet.java
@@ -23,14 +23,12 @@ public class ConversionServlet extends HttpServlet {
      */
     public ConversionServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
